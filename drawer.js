6ff8c18d9f326907666b574fbf69adf34a846b06
Oleg <oleg@Alexs-Severinov-Mac-mini-2.local>
2014-11-03 19:00:03 +0200

    second commit

--- drawer.js
+++ drawer.js
@@ -9,20 +9,27 @@ Drawer.prototype.drawAll = function (data) {
         x1 = 0,
         stepX = 64,
         y1 = 0,
-        stepY = 48;
+        stepY = 48,
+        w = 11,
+        h = 11;
     canvas.width = 640;
     canvas.height = 480;
     canvas.style.border = '1px solid';
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "black";
-    console.log(data.length);
-   for (var i =0; i<data.length; i++){
+    for (var i =0; i<data.length; i++){
+
+        var x = i%w;
+        var y = Math.floor(i/h);
+        var xG = x*stepX;
+        var yG = y*stepY;
+        console.log(xG);
+        console.log(yG);
         ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(stepX, stepY);
+        ctx.moveTo(xG, yG);
+        ctx.lineTo(stepX+xG, stepY+yG);
         ctx.stroke();
-        x1 += stepX;
-        y1 += stepY;
+
     }
 
 };
