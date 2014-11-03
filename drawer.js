608f32b1409266b16b650972c7a46e6b1fa939fa
Lol9tor <lol9tor@gmail.com>
2014-11-03 23:42:22 +0200

    maze without exit

--- drawer.js
+++ drawer.js
@@ -1,7 +1,6 @@
 
 function Drawer () {
 
-
 }
 
 Drawer.prototype.drawAll = function (data) {
@@ -14,22 +13,31 @@ Drawer.prototype.drawAll = function (data) {
         h = 11;
     canvas.width = 640;
     canvas.height = 480;
-    canvas.style.border = '1px solid';
+    //canvas.style.border = '1px solid';
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "black";
+
     for (var i =0; i<data.length; i++){
+        var x = i%w,
+            y = Math.floor(i/h),
+            xG = x*stepX,
+            yG = y*stepY;
 
-        var x = i%w;
-        var y = Math.floor(i/h);
-        var xG = x*stepX;
-        var yG = y*stepY;
-        console.log(xG);
-        console.log(yG);
-        ctx.beginPath();
-        ctx.moveTo(xG, yG);
-        ctx.lineTo(stepX+xG, stepY+yG);
-        ctx.stroke();
-
+        if (data[i].left){
+            ctx.beginPath();
+            ctx.moveTo(xG, yG);
+            ctx.lineTo(xG, stepY+yG);
+            ctx.stroke();
+        }
+        if (data[i].top){
+            ctx.beginPath();
+            ctx.moveTo(xG, yG);
+            ctx.lineTo(stepX+xG, yG);
+            ctx.stroke();
+        }
     }
+    this.compareCells(data);
+};
+Drawer.prototype.compareCells = function (data) {
 
 };
